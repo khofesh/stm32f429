6958290216c897b3bf4f1c57a0342b69aaecafa7
fahmi ahmad <fahmiahmad@tutanota.com>
2023-10-28 18:25:17 +0700

    logger helper

--- usb-firmware/usb_device/Src/main.c
+++ usb-firmware/usb_device/Src/main.c
@@ -22,12 +22,17 @@
 
 #include <stdint.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include "stm32f429xx.h"
+
+#include "Helpers/logger.h"
+
+#define ever			;;
+
 
 int main(void)
 {
+	log_info("program entrypoint");
+
     /* Loop forever */
-	for(;;);
+	for(ever);
 }
---
+++ usb-firmware/usb_device/Src/systeminit.c
@@ -0,0 +1,15 @@
+/*
+ * systeminit.c
+ *
+ *  Created on: Oct 28, 2023
+ *      Author: fahmad
+ */
+
+#include "Helpers/logger.h"
+
+LogLevel system_log_level = LOG_LEVEL_DEBUG;
+
+void SystemInit(void)
+{
+
+}
